15c7bdc7ae1b74acadf56a5c5c64b22ee47ccfc9
cassj <cassjohnston@gmail.com>
2011-06-29 15:50:34 +0100

    generate peak counts seperately. First go at DeSeq.

--- compare_peaks.R
+++ compare_peaks.R
@@ -9,24 +9,10 @@
 # A BED file containing the peak regions for each of the samples
 
 
-#!/usr/local/bin/Rscript compare_peaks /path/to/outdir N_threads /path/to/bam1 /path/to/bed1 /path/to/bam2 /path/to/bed2 ...
+#!/usr/local/bin/Rscript compare_peaks /path/to/counts_file.RData /path/to/libsizes.RData
 
 options(stringsAsFactors = FALSE);
-
-
 args <- commandArgs(trailingOnly=TRUE)
-
-#for testing
-args<-c("/mnt/data/",4, "/mnt/astro/BAM/IP.bam" ,"/mnt/astro/Macs/NA_peaks.bed", "/mnt/esc/chip_export_sorted_nodups.bam","/mnt/esc/macs_300_1.0e-05/EscChIPseqREST_peaks.bed")
-
-resdir <- args[1]
-threads <- args[2]
-args <- args[-1:-2]
-
-inds <- 1:length(args)
-bam.files <- args[which(inds%%2!=0)]
-bed.files <- args[which(inds%%2==0)]
-
 
 library(IRanges)
 library(ShortRead)
@@ -35,9 +21,69 @@ library(baySeq)
 library(DESeq)
 library(rtracklayer)
 library(Rsamtools)
+library(edgeR)
 
-# For ChIPseq data we aren't dealing with that many locations, so probably
-# we're good using just the number of cores on the AWS machine. 
+counts.file <- args[1]
+libsizes.file <- args[2]
+
+#for testing
+counts.file <- "/mnt/data/counts.RData"
+libsizes.file <- "/mnt/data/libsizes.RData"
+
+counts <- get(load(counts.file))
+libsizes <- get(load(libsizes.file))
+libsizes <- as.integer(libsizes)
+
+# assuming no reps at the moment
+groups <- 1:(ncol(counts) - 1)
+
+
+### edgeR
+
+# I think, as we have no reps, we get common dispersion of 0 which is essentialy Poisson.
+# Which means the p values are far too low cos the Poisson doesn't model it very well.
+d <- DGEList(counts[,-1], group=groups)
+
+# Can we use the input sample to generate a null dist?
+
+#unsure if we need to worry about normalisation. Supposed to correct for highly expressed transcripts. Don't think we're dealing with the same range in chip
+#Ignore this step for now
+
+
+#Estimating dispersion for the NB distribution
+#quantile adjusted conditional maximum likelihood (qCML) for single factors
+#Cox-Reid Profile-adjusted likelihood (CR) for multiple factors
+
+
+# DE
+# exact test -> based on qCML
+# GLM
+
+### DESeq
+
+
+counts.table <- counts[,-1]
+group.names <- c("TC","SC")
+
+counts.table <- counts[,-1]
+counts.table <- counts.table + 1
+cds <- newCountDataSet( counts.table, group.names)
+cds <- estimateSizeFactors(cds)
+cds <- estimateVarianceFunctions(cds, method="blind")
+res <- nbinomTest(cds, "TC","SC")
+
+res <- res[order(res[,"pval"]),]
+
+
+# need to map res back to genes, but it kinda works.
+# Really not getting enough sig with no reps though.
+
+
+# ok, this kinda works but we're getting infinite fold changes. How to deal with the 0 values?
+
+### baySeq
+
+# can't get this to work with multiple threads
 if(is.null(threads) || threads==1){
   cl <- NULL
 }else{
@@ -45,43 +91,6 @@ if(is.null(threads) || threads==1){
 }
 
 
-# Read in theBED files as RangedData.
-# Retrieve the read data for those regions
-# Build the counts table
-# This seems to take about an hour for 4K regions.
-
-beds <- list()
-counts <- NULL
-seglens <- NULL
-for(i in 1:length(bed.files)){
-  beds[[i]] <- import(bed.files[i])
-
-  #get the count data for these ranges from each bam file
-  bam.counts <- list()
-  for(j in 1:length(bam.files)){
-    what <- c("qname") 
-    param <- ScanBamParam(which=beds[[i]], what=what)
-    bam <- scanBam(bam.files[[j]], param=param)
-    bam.counts[[j]] <- sapply(bam, function(x){length(x$qname)})
-  }
-
-  beds[[i]] <- as.data.frame(beds[[i]])
-  nms <- paste(beds[[i]][,"space"], paste(beds[[i]][,"start"], beds[[i]][,"end"], sep="-"), sep=":")
-  these <- do.call(cbind, bam.counts)
-  colnames(these) <- bam.files
-  if(is.null(counts)){
-    counts <- these
-  }else{
-    counts <- rbind(counts, these)
-  }
-  if(is.null(seglens)){
-    seglens <- beds[[i]][,"end"]-beds[[i]][,"start"]+1
-  }else{
-    seglens <- c(seglens,beds[[i]][,"end"]-beds[[i]][,"start"]+1)
-  }
-}
-
-save(counts, file=paste(resdir,"counts.RData", sep="/"))
 
 # Now use baySeq to determine differentially expressed counts.
 replicates <- c(1,2)
@@ -102,7 +111,7 @@ cd <- new("countData", data=counts, replicates=replicates, libsizes=as.integer(l
 
 # Bootstrap an empirical distribution from the data.
 # This should really use 10,000 iterations, Maybe best to use the multiple cores
-NBML <- getPriors.NB(cd, samplesize=1000, estimation="QL", cl=cl)
+NBML <- getPriors.NB(cd, samplesize=10000, estimation="QL", cl=cl)
 post.NBML <- getLikelihoods.NB(NBML,pET="BIC",cl=cl)
 
 props <- post.NBML@estProps
---
+++ peak_read_counts.R
@@ -0,0 +1,100 @@
+###
+#
+# For each region in the BED files, fetches the read count in that regions from the BAM file.
+#
+###
+
+# you will need:
+# The BAM files for each of the samples
+# A BED file containing the peak regions for each of the samples
+
+
+#!/usr/local/bin/Rscript peak_read_counts.R /path/to/outdir N_threads /path/to/bam1 /path/to/bed1 /path/to/bam2 /path/to/bed2 ...
+
+options(stringsAsFactors = FALSE);
+
+
+args <- commandArgs(trailingOnly=TRUE)
+
+#for testing
+#args<-c("/mnt/data/",4, "/mnt/astro/BAM/IP.bam" ,"/mnt/astro/Macs/NA_peaks.bed", "/mnt/esc/chip_export_sorted_nodups.bam","/mnt/esc/macs_300_1.0e-05/EscChIPseqREST_peaks.bed")
+args <- c("/mnt/data/",4,"/mnt/TC/CMN066_s_8_export_sorted_nodups.bam", "/mnt/TC/macs_300_1.0e-05/Mash1_TC_peaks.bed", "/mnt/SC/chip_export_sorted_nodups.bam", "/mnt/SC/macs_300_1.0e-05/Mash1_SC_peaks.bed")
+
+
+resdir <- args[1]
+threads <- args[2]
+args <- args[-1:-2]
+
+inds <- 1:length(args)
+bam.files <- args[which(inds%%2!=0)]
+bed.files <- args[which(inds%%2==0)]
+
+
+library(IRanges)
+library(ShortRead)
+library(snow)
+library(baySeq)
+library(DESeq)
+library(rtracklayer)
+library(Rsamtools)
+
+# For ChIPseq data we aren't dealing with that many locations, so probably
+# we're good using just the number of cores on the AWS machine. 
+if(is.null(threads) || threads==1){
+  cl <- NULL
+}else{
+  cl <- makeCluster(threads,"SOCK")
+}
+
+
+# Read in the BED files as RangedData.
+# Retrieve the read data for those regions
+# Build the counts table
+# This seems to take about an hour for 4K regions.
+
+beds <- list()
+counts <- NULL
+seglens <- NULL
+for(i in 1:length(bed.files)){
+  beds[[i]] <- import(bed.files[i])
+
+  #get the count data for these ranges from each bam file
+  bam.counts <- list()
+  for(j in 1:length(bam.files)){
+    what <- c("qname") 
+    param <- ScanBamParam(which=beds[[i]], what=what)
+    bam <- scanBam(bam.files[[j]], param=param)
+    bam.counts[[j]] <- sapply(bam, function(x){length(x$qname)})
+  }
+
+  beds[[i]] <- as.data.frame(beds[[i]])
+  nms <- paste(beds[[i]][,"space"], paste(beds[[i]][,"start"], beds[[i]][,"end"], sep="-"), sep=":")
+  these <- do.call(cbind, bam.counts)
+  col.nms <- sub("-","", gsub("/","-", bam.files))
+  colnames(these) <- col.nms
+  these <- data.frame(sample=bed.files[i], these)
+  if(is.null(counts)){
+    counts <- these
+  }else{
+    counts <- rbind(counts, these)
+  }
+  if(is.null(seglens)){
+    seglens <- beds[[i]][,"end"]-beds[[i]][,"start"]+1
+  }else{
+    seglens <- c(seglens,beds[[i]][,"end"]-beds[[i]][,"start"]+1)
+  }
+}
+
+save(counts, file=paste(resdir,"counts.RData", sep="/"))
+
+
+
+# We'll need the library sizes later, so get them from the summary files
+summaries <- gsub('.bam', '.summary', bam.files )
+libsizes <- sapply(summaries, function(x){
+  l <- readLines(x,1)
+  l <- sub("\\s+.*","",l, perl=T)
+})
+
+
+save(libsizes, file=paste(resdir,"libsizes.RData", sep="/"))
